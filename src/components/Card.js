250f4b056a295553801794475f0bd2ee1e3dbc12
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-12-01 13:49:03 -0500

    updates card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -20,7 +20,6 @@ const Card = ({ props }) => {
         <>
             <div class="card text-center" >
                 <h1 style={{ fontSize: '5rem' }}>{props.emoji}</h1>
-                <img class="card-img-top" src={props.gif ? props.gif : ''} alt={`emoji of ${props.name}`} />
                 <div class="card-body">
                     <h5 class="card-title">{props.name}</h5>
                     <p class="card-text">{props.mainText}</p>
